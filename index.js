2d6346cc21554323bdab4e9a87684f8fbac34af8
tangusjr <kipngenogilbert96@gmail.com>
2022-06-25 19:25:36 +0300

    added CSS styling to the background and content

---
+++ index.js
@@ -0,0 +1,28 @@
+const API = "33bdb448f27cb420996ffe4512c31214";
+window.addEventListener("load", () => {});
+
+window.addEventListener("load", () => {
+  let long;
+  let lat;
+  // Accessing Geolocation of User
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition((position) => {
+      long = position.coords.longitude;
+      lat = position.coords.latitude;
+
+      const base = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${api}&units=metric`;
+
+      // Using fetch to get data
+      fetch(base)
+        .then((response) => {
+          return response.json();
+        })
+        .then((data) => {
+          const { temp } = data.main;
+          const place = data.name;
+          const { description, icon } = data.weather[0];
+          const { sunrise, sunset } = data.sys;
+        });
+    });
+  }
+});
