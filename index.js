d5aba3847300c260d7044f554921a5e1fbc4bdf8
tangusjr <kipngenogilbert96@gmail.com>
2022-06-26 11:15:35 +0300

    grab dom elements and storing them in a variable

--- index.js
+++ index.js
@@ -1,53 +1,131 @@
-const API = "33bdb448f27cb420996ffe4512c31214";
-window.addEventListener("load", () => {});
+const setAPIKey = "33bdb448f27cb420996ffe4512c31214";
+const days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+const months = [
+  "Jan",
+  "Feb",
+  "Mar",
+  "Apr",
+  "May",
+  "Jun",
+  "Jul",
+  "Aug",
+  "Sep",
+  "Oct",
+  "Nov",
+  "Dec",
+];
 
-window.addEventListener("load", () => {
-  let long;
-  let lat;
-  // Accessing Geolocation of User
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition((position) => {
-      long = position.coords.longitude;
-      lat = position.coords.latitude;
+const timeProperty = document.getElementById("time");
+const dateProperty = document.getElementById("date");
+const currentWeatherItems = document.getElementById("current-weather-items");
+const timezone = document.getElementById("time-zone");
+const country = document.getElementById("country");
+const weatherForecast = document.getElementById("weather-forecast");
+const currentTemp = document.getElementById("current-temp");
 
-      const baseAPI = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${API}&units=metric`;
+setInterval(() => {
+  const time = new Date();
+  const month = time.getMonth();
+  const date = time.getDate();
+  const day = time.getDay();
+  const hour = time.getHours();
+  const timeIn12Hr = hour >= 13 ? hour % 12 : hour;
+  const minutes = time.getMinutes();
+  const amPm = hour >= 12 ? "PM" : "AM";
 
-      // Using fetch to get data
-      fetch(baseAPI)
-        .then((res) => {
-          return res.json();
-        })
-        .then((data) => {
-          const { temp } = data.main;
-          const place = data.name;
-          const { description, icon } = data.weather[0];
-          const { morning, evening } = data.sys;
+  timeProperty.innerHTML =
+    (timeIn12Hr < 10 ? "0" + timeIn12Hr : timeIn12Hr) +
+    ":" +
+    (minutes < 10 ? "0" + minutes : minutes) +
+    " " +
+    `<span id="am-pm">${ampm}</span>`;
 
-          const urlOfIcon = `http://openweathermap.org/img/wn/${icon}@2x.png`;
-          const fahrenheit = (temp * 9) / 5 + 32;
+  dateProperty.innerHTML = days[day] + ", " + date + " " + months[month];
+}, 1000);
 
-          // Converting Epoch(Unix) time to GMT
-          const morningGMT = new Date(morning * 1000);
-          const eveningGMT = new Date(evening * 1000);
+retrieveWeatherData();
+function retrieveWeatherData() {
+  navigator.geolocation.getCurrentPosition((successful) => {
+    let { latitude, longitude } = successful.coords;
 
-        });
-    });
-  }
-});
+    fetch(
+      `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude=hourly,minutely&units=metric&appid=${setAPIKey}`
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        displayWeatherData(data);
+      });
+  });
+}
+function displayWeatherData(data) {
+  let { humidity, pressure, sunrise, sunset, wind_speed } = data.current;
 
-const timeInGMT = new Date(epochTime * 1000);
+  timezone.innerHTML = data.timezone;
+  countryProperty.innerHTML = data.lat + "N " + data.lon + "E";
 
-const iconImg = document.getElementById("weather-icon");
-const loc = document.querySelector("#location");
-const tempC = document.querySelector(".c");
-const tempF = document.querySelector(".f");
-const desc = document.querySelector(".desc");
-const sunriseDOM = document.querySelector(".sunrise");
-const sunsetDOM = document.querySelector(".sunset");
+  currentWeatherItems.innerHTML = `<div class="weather-item">
+      <div>Humidity</div>
+      <div>${humidity}%</div>
+  </div>
+  <div class="weather-item">
+      <div>Pressure</div>
+      <div>${pressure}</div>
+  </div>
+  <div class="weather-item">
+      <div>Wind Speed</div>
+      <div>${wind_speed}</div>
+  </div>
+  <div class="weather-item">
+      <div>Sunrise</div>
+      <div>${window.moment(sunrise * 1000).format("HH:mm a")}</div>
+  </div>
+  <div class="weather-item">
+      <div>Sunset</div>
+      <div>${window.moment(sunset * 1000).format("HH:mm a")}</div>
+  </div> 
+  `;
 
-loc.textContent = `${place}`;
-desc.textContent = `${description}`;
-tempC.textContent = `${temp.toFixed(2)} °C`;
-tempF.textContent = `${fahrenheit.toFixed(2)} °F`;
-sunriseDOM.textContent = `${morningGMT.toLocaleDateString()}, ${morningGMT.toLocaleTimeString()}`;
-sunsetDOM.textContent = `${eveningGMT.toLocaleDateString()}, ${eveningGMT.toLocaleTimeString()}`;
+  let otherDayForecast = "";
+  data.daily.forEach((day, index) => {
+    if (index == 0) {
+      currentTemp.innerHTML = `
+          <img src="http://openweathermap.org/img/wn//${
+            day.weather[0].icon
+          }@4x.png" alt="weather icon" class="w-icon">
+          <div class="other">
+              <div class="day">${window
+                .moment(day.dt * 1000)
+                .format("dddd")}</div>
+              <div class="temp">Night - ${day.temp.night}&#176;C</div>
+              <div class="temp">Day - ${day.temp.day}&#176;C</div>
+          </div>
+          
+          `;
+    } else {
+      otherDayForecast += `
+          <div class="weather-forecast-item">
+              <div class="day">${window
+                .moment(day.dt * 1000)
+                .format("ddd")}</div>
+              <img src="http://openweathermap.org/img/wn/${
+                day.weather[0].icon
+              }@2x.png" alt="weather icon" class="w-icon">
+              <div class="temp">Night - ${day.temp.night}&#176;C</div>
+              <div class="temp">Day - ${day.temp.day}&#176;C</div>
+          </div>
+          
+          `;
+    }
+  });
+
+  weatherForecast.innerHTML = otherDayForecast;
+}
